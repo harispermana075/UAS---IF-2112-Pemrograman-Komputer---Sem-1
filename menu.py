118246da0d0875e64921bb027f106711ab6f274d
harispermana075 <95922307+harispermana075@users.noreply.github.com>
2021-12-17 08:31:35 +0700

    Update menu.py

--- menu.py
+++ menu.py
@@ -5,11 +5,11 @@ import soalc as c
 
 opsi = st.sidebar.selectbox("Fitur Aplikasi", ("Produksi Minyak Mentah Tahunan", "Produksi Terbesar pada Tahun Tertentu", "Produksi Minyak Mentah Kumulatif", "Informasi Negara"))
 
-if opsi == "Produksi Minyak Mentah Tahunan":
+if opsi == "Produksi Minyak Mentah Tahunan Negara":
     a.minyak()
-elif opsi == "Produksi Terbesar pada Tahun Tertentu":
+elif opsi == "Produksi Minyak Mentah Terbesar pada Tahun Tertentu":
     b.terbesar()
-elif opsi == "Produksi Minyak Mentah Kumulatif":
+elif opsi == "Produksi Minyak Mentah Kumulatif Terbesar":
     c.kumulatif()
-elif opsi == "Informasi Negara":
+elif opsi == "Informasi Negara Tertentu serta Data Produksi Minyak Mentah":
     a.info_negara()
